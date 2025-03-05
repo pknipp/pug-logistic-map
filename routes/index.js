182b662cf4e5ea4f07bd446021b3586e1ac7e30f
Peter Knipp <peteraknipp@gmail.com>
2025-03-05 11:42:22 -0500

    Change /json/ to /api/ and /svg/ to /

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 router.get("", (req, res) => res.render('home'));
-["json", "svg"].forEach(route => router.use(`/${route}`, require(`./${route}`)));
+// ["json", "svg"].forEach(route => router.use(`/${route}`, require(`./${route}`)));
+router.use("/", require("./svg"));
+router.use("/api", require("./json"));
 module.exports = router;
